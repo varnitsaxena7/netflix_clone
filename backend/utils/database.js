5c180d5e9dd983a5b38730e6fb9ddc70e8b47e67
varnitsaxena7 <118539746+varnitsaxena7@users.noreply.github.com>
2024-06-17 02:50:10 +0530

    Update database.js

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -6,7 +6,7 @@ dotenv.config({
 })
 
 const databaseConnection = () => {
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
+    mongoose.connect("mongodb+srv://saxenavarnitcode7:hello123@cluster0.4tu17jt.mongodb.net/netflixx?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
         console.log("mongodb connected successfully");
     }).catch((error)=>{
         console.log(error);
